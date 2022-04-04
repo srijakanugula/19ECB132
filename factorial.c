03bf1e99d6d89e1897ae934d388ed04c1d07104a
SRIJA KANUGULA KANUGULA <101540347+srijakanugula@users.noreply.github.com>
2022-04-04 04:36:24 +0000

    factorial

---
+++ factorial.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+    int i, factorial=1 , num;
+   printf(" enter a number");
+   scanf ("%d, &num");
+   if (num>10)
+        printf ("too big a number");
+        else if (num<0)
+        printf("negative number");
+        else{for( i<1; i <=n; i++)
+         f=f*i;
+           printf ("factorial is %d",f);
+        }
+        return 0;
+    }
