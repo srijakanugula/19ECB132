e6c45f43966315b41c4dec02ae95868e850f6780
SRIJA KANUGULA KANUGULA <101540347+srijakanugula@users.noreply.github.com>
2022-05-31 20:15:09 +0000

    ...

--- matrix.c
+++ matrix.c
@@ -16,3 +16,23 @@ int main()
   printf("\n");}
   return 0;
   }
+*/
+how many rows do you want:4
+how many columns do you want:5
+enter elements in [0][0]:2 5
+enter elements in [0][1]:enter elements in [0][2]:86
+enter elements in [0][3]:7 9 
+enter elements in [0][4]:enter elements in [1][0]: 6 3 
+enter elements in [1][1]:enter elements in [1][2]:3 5
+enter elements in [1][3]:enter elements in [1][4]:33
+enter elements in [2][0]:32
+enter elements in [2][1]:9 0
+enter elements in [2][2]:enter elements in [2][3]: 2 4
+enter elements in [2][4]:enter elements in [3][0]:1 2
+enter elements in [3][1]:enter elements in [3][2]:1 1
+enter elements in [3][3]:enter elements in [3][4]:5 6 
+2   5   86  7   9
+6   3   3   5   33
+32  9   0   2   4
+1   2   1   1   5
+  /*
