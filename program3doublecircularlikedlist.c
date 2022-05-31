1892cd5d51d4a89a2a08b614cc98f223ed6d1b80
SRIJA KANUGULA KANUGULA <101540347+srijakanugula@users.noreply.github.com>
2022-05-31 20:31:42 +0000

    ...

--- program3doublecircularlikedlist.c
+++ program3doublecircularlikedlist.c
@@ -241,3 +241,122 @@ void search()
         }  
     }          
 }  
+*/
+output:
+*********Main Menu*********
+
+Choose one option from the following list ...
+
+===============================================
+
+1.Insert in Beginning
+2.Insert at last
+3.Delete from Beginning
+4.Delete from last
+5.Search
+6.Show
+7.Exit
+
+Enter your choice?
+2
+
+Enter value 67
+
+node inserted
+
+*********Main Menu*********
+
+Choose one option from the following list ...
+
+===============================================
+
+1.Insert in Beginning
+2.Insert at last
+3.Delete from Beginning
+4.Delete from last
+5.Search
+6.Show
+7.Exit
+
+Enter your choice?
+1
+
+Enter Item value 8
+
+Node inserted
+
+*********Main Menu*********
+
+Choose one option from the following list ...
+
+===============================================
+
+1.Insert in Beginning
+2.Insert at last
+3.Delete from Beginning
+4.Delete from last
+5.Search
+6.Show
+7.Exit
+
+Enter your choice?
+5
+
+Enter item which you want to search?
+8
+item found at location 1
+*********Main Menu*********
+
+Choose one option from the following list ...
+
+===============================================
+
+1.Insert in Beginning
+2.Insert at last
+3.Delete from Beginning
+4.Delete from last
+5.Search
+6.Show
+7.Exit
+
+Enter your choice?
+6
+
+ printing values ... 
+8
+67
+
+*********Main Menu*********
+
+Choose one option from the following list ...
+
+===============================================
+
+1.Insert in Beginning
+2.Insert at last
+3.Delete from Beginning
+4.Delete from last
+5.Search
+6.Show
+7.Exit
+
+Enter your choice?
+67
+Please enter valid choice..
+*********Main Menu*********
+
+Choose one option from the following list ...
+
+===============================================
+
+1.Insert in Beginning
+2.Insert at last
+3.Delete from Beginning
+4.Delete from last
+5.Search
+6.Show
+7.Exit
+
+Enter your choice?
+7
+  /*
