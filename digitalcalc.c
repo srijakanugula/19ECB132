fc871d89389630ac5621fb7f3d23f687802de8b3
SRIJA KANUGULA KANUGULA <101540347+srijakanugula@users.noreply.github.com>
2022-05-31 20:23:44 +0000

    ...

--- digitalcalc.c
+++ digitalcalc.c
@@ -2,9 +2,9 @@
  int main()
  {char opr;
  float n1,n2,ans;
- printf("what operation do you want to erform");
+ printf("what operation do you want to perform");
  scanf("%c",&opr);
- printf("enter two numbers"); scanf("%f %f",&ni,&n2);
+ printf("enter two numbers"); scanf("%f %f",&n1,&n2);
  switch (opr)
  {case '+':
  ans=n1+n2;
@@ -20,3 +20,10 @@
  break;}
  return 0;
  } 
+*/
+output:
+./a.out
+what operation do you want to perform div
+enter two numbers~ 22 11
+  ans=2
+  /*
